b8c2a24af14199a2f6c33f981de67b2c03682d25
Moesjarraf J <moesjarraf.jahangier@hva.nl>
2014-06-02 02:15:53 +0200

    added purchase history and more

--- js/cart.js
+++ js/cart.js
@@ -77,7 +77,7 @@ function addToCart(product)
 
     $("#scannedProduct").popup("close");
     var numericInput = "<div class='numericInput' style='height: 30px !important; width: 94px !important; border: none !important;'><div class='niControlBox'><a class='niPlus niButton' style='height: 30px !important; width: 30px !important; line-height: 30px !important;'>+</a><a class='niMinus niButton' style='height: 30px !important; width: 30px !important; line-height: 30px !important;'>-</a></div><input class='niInput tableTextfield' style='height: 28px !important; width: 31px !important; text-align: center !important;' type='text' value='" + product.quantity + "'></div>";
-    $("#tableCart tbody").append("<tr class='border'><td class='item_code tableHeader smallRow noBorder' style='border-right: 1px solid black !important;'><input class='tableTextfield' type='hidden' value='" + product.item_code + "'>" + product.item_name + "</td><td class='quantity noBorder' style='width: 93px;'>" + numericInput + "</td><td class='noBorder' style='width: 65px; border-left: 1px solid black !important;'><a class='ui-btn tableButton maxWidth red' onclick='removeFromCart(this);'>Remove</a></td></tr>");
+    $("#tableCart tbody").append("<tr class='border'><td class='item_code tableHeader smallRow noBorder' style='border-right: 1px solid black !important;'><input class='tableTextfield' type='hidden' value='" + product.item_code + "'>" + product.item_name + "</td><td class='quantity noBorder' style='width: 93px;'>" + numericInput + "</td><td class='noBorder hiddenDataCell' style='width: 65px; border-left: 1px solid black !important;'><a class='ui-btn tableButton maxWidth red' onclick='removeFromCart(this);'>Remove</a><input class='hiItemName' type='hidden' value='" + product.item_name + "'/><input class='hiBarcode' type='hidden' value='" + product.barcode + "'/><input class='hiBrand' type='hidden' value='" + product.brand + "'/><input class='hiDescription' type='hidden' value='" + product.description + "'/><input class='hiImage' type='hidden' value='" + product.image + "'/><input class='hiSupplier' type='hidden' value='" + product.default_supplier + "'/></td></tr>");
     checkCartEmpty();
 }
 
@@ -122,7 +122,8 @@ function addCartToOrder()
                     {
                         $("#selectSupplier").append("<option value='" + i.name + "'>" + i.name + "</option>");
                     });;
-
+                    $("#selectSupplier")[0].selectedIndex = 0;
+                    $("#selectSupplier").selectmenu("refresh", true);
                     $("#showCustomizeOrder").trigger("click");
                 }
                 else
@@ -156,13 +157,18 @@ function sendOrder()
     $("#tableCart tr:gt(0)").each(function()
     {
         var item_code = $(this).find(".item_code").find(".tableTextfield").val();
+        var description = $(this).find(".hiddenDataCell").find(".hiDescription").val();
+        var item_name = $(this).find(".hiddenDataCell").find(".hiItemName").val();
+        var brand = $(this).find(".hiddenDataCell").find(".hiBrand").val();
+        var barcode = $(this).find(".hiddenDataCell").find(".hiBarcode").val();
+        var image = $(this).find(".hiddenDataCell").find(".hiImage").val();
+        var supplier = $(this).find(".hiddenDataCell").find(".hiSupplier").val();
         var quantity = $(this).find(".quantity").find(".tableTextfield").val();
-        po_details.push({"item_code" : item_code, "qty" : quantity, "rate" : 0, "schedule_date" : date});
+        po_details.push({"item_code" : item_code, "item_name" : item_name, "qty" : quantity, "schedule_date" : date, "barcode" : barcode, "brand" : brand, "description" : description, "image" : image, "default_supplier" : supplier});
     });
 
     data["po_details"] = po_details;
     $.mobile.loading('show')
-
     socket.emit("insertPurchaseOrder", data,
     function(callback)
     {
--- js/purchase_history.js
+++ js/purchase_history.js
@@ -1,26 +1,25 @@
-$(document).on("pageshow", "#items", function()
+$(document).on("pageshow", "#purchase_history", function()
 {
-    $(".searchbox").val("");
-    searchItems();
+    getPurchaseHistory();
 });
 
-function searchItems()
+function getPurchaseHistory()
 {
     $.mobile.loading("show");
 
-    socket.emit("getItems", function(callback)
+    socket.emit("getPurchaseHistory", function(callback)
     {
-        $("#tableItems tbody tr").remove();
+        $("#tablePurchaseHistory tbody tr").remove();
         
         if(callback != null)
         {
-            callback.data.forEach(function(i)
+            callback.forEach(function(i)
             {
                 var numericInput = $.parseHTML("<div class='numericInput absolute absBottomRight'><div class='niControlBox'><a class='niPlus niButton niCartPlus'>+</a><a class='niMinus niButton niCartMinus'>-</a></div><input class='niInput niCartInput' type='text' value='0'></div>");
-                var tableRow = $.parseHTML("<tr class='selectable'><td class='imageColumn searchbox-cell'><input class='it_item_name searchbox-textfield' type='hidden' value='" + i.item_name + "'/><input type='hidden' class='it_item_code' value='" + i.item_code + "'/><input class='it_image' type='hidden' value='" + i.image + "'/><input class='it_barcode' type='hidden' value='" + i.barcode + "'/><input class='it_description' type='hidden' value='" + i.description + "'/><input class='it_brand' type='hidden' value='" + i.brand + "'/><input class='it_supplier' type='hidden' value='" + i.default_supplier + "'/><div class='image'></div></td><td><div class='itemHeader'><span class='brand'>" + i.brand + "</span> - " + i.item_name + " <span class='smallPrice'>[" + i.barcode + "]</span></div><div class='itemDescription wordwrap' style='position: relative !important;'>" + i.description + "<span class='smallPrice' style='position: absolute; bottom: 3px; left: 8px;'>" + i.default_supplier + "</span></div></td></tr>");
+                var tableRow = $.parseHTML("<tr class='selectable'><td class='imageColumn searchbox-cell'><input class='it_item_name searchbox-textfield' type='hidden' value='" + i.item_name + "'/><input type='hidden' class='it_item_code' value='" + i.item_code + "'/><input class='it_image' type='hidden' value='" + i.image + "'/><input class='it_barcode' type='hidden' value='" + i.barcode + "'/><input class='it_description' type='hidden' value='" + i.description + "'/><input class='it_brand' type='hidden' value='" + i.brand + "'/><input class='it_supplier' type='hidden' value='" + i.default_supplier + "'/><div class='image'></div></td><td><div class='itemHeader'><span class='brand'>" + i.brand + "</span> - " + i.item_name + " <span class='smallPrice'>[" + i.barcode + "]</span></div><div class='itemDescription wordwrap' style='position: relative !important;'>" + i.description + "<span class='smallPrice' style='position: absolute; bottom: 3px; left: 8px;'>" + i.default_supplier + " - <span class='greenBrand'>" + i.qty + "x</span></span></div></td></tr>");
                 $(tableRow).find(".itemDescription").append(numericInput);
 
-                if(i.image != null)
+                if(i.image != "null")
                 {
                     var image = i.image;
 
@@ -48,23 +47,23 @@ function searchItems()
                     });
                 }
 
-                $("#tableItems tbody").append(tableRow);
+                $("#tablePurchaseHistory tbody").append(tableRow);
             });
 
-            $.mobile.changePage("#items", {transition : "slide"});
+            $.mobile.changePage("#purchase_history", {transition : "slide"});
         }
         else
         {
-            alert("Failed to get the list of items. Please try again later.");
+            alert("Failed to get your purchase history. Please try again later.");
         }
 
         $.mobile.loading("hide");
     });
 }
 
-function addItems()
+function addPHToCart()
 {
-    $("#tableItems tbody tr").each(function()
+    $("#tablePurchaseHistory tbody tr").each(function()
     {
         if($(this).hasClass("selected"))
         {
